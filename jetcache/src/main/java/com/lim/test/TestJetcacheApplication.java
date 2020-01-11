bbf333272999a66fa8efcf5ef3e227cfb9ca0d8c
lim <1443549902@qq.com>
2020-01-11 22:24:24 +0800

    [+] init test project for spring cloud

---
+++ jetcache/src/main/java/com/lim/test/TestJetcacheApplication.java
@@ -0,0 +1,13 @@
+package com.lim.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJetcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJetcacheApplication.class, args);
+    }
+
+}
---
+++ jetcache/src/test/java/com/lim/test/TestJetcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJetcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
