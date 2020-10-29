56a92862fce2f037a70fe0de373f6f5534941205
Li Ming <liming@tp-link.com.cn>
2020-10-29 11:37:00 +0800

    Module Refactoring
    1.调整base模块代码

--- base/src/main/java/com/lim/test/base/BaseApplication.java
+++ base/src/main/java/com/lim/test/base/BaseApplication.java
@@ -3,11 +3,18 @@ package com.lim.test.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 基础springboot测试模块
+ * 用于复制修改生成其他测试模块
+ *
+ * @author lim
+ * @since 2020/1/11
+ */
 @SpringBootApplication
-public class TestBaseApplication {
+public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestBaseApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 
 }
--- base/src/main/java/com/lim/test/base/demo/controller/DemoController.java
+++ base/src/main/java/com/lim/test/base/demo/controller/DemoController.java
@@ -1,19 +1,24 @@
 package com.lim.test.base.demo.controller;
 
+import com.lim.test.tools.commonresponse.response.ResponseBody;
+import com.lim.test.tools.commonresponse.response.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Lim
- * @date 2020/1/11
+ * 测试实例controller
+ *
+ * @author lim
+ * @since 2020/1/11
  */
 @RestController
 @RequestMapping("/test")
 public class DemoController {
 
     @GetMapping
-    public String getTest() {
-        return "getTest";
+    public ResponseBody<String> getTest() {
+        return Result.success("getTest");
     }
+
 }
